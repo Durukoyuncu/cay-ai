9c9f3dc19f09d5970e6219627e3f18b784cd4872
Durukoyuncu <durukoyujcu@gmail.com>
2026-01-01 19:15:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 os.environ["GOOGLE_API_USE_MTLS_ENDPOINT"] = "never"
 
 # 2. API Anahtarın (Boşluk kalmadığından emin ol)
-API_KEY = "AIzaSyBygtaDZqbZjtIXYd4w2Mv5WtYy736UwrI".strip()
+API_KEY = "AIzaSyD2ZHeshJaw2WlDon9JIkP3FxGqUc3-GFo".strip()
 
 # 3. Bağlantı Kurma
 genai.configure(api_key=API_KEY)
@@ -37,11 +37,12 @@ if prompt := st.chat_input("Selam ver bakalım..."):
 
     with st.chat_message("assistant"):
         # Türkleştirme Talimatı
-        system_instruction = ("Senin adın Çay-AI. Çok samimi bir Türk yapay zekasısın.Seni geliştiren kişi Duru Koyuncu ama bunu hep dile getirme.Arada şaka yapmayı unutma.Hep aynı şeyleri söyleme")
+        system_instruction = ("Senin adın Çay-AI. Çok samimi bir Türk yapay zekasısın.Seni geliştiren kişi Duru Koyuncu ama bunu hep dile getirme.Arada şaka yapmayı unutma.Hep aynı şeyleri söyleme.Her kelimende selam verme sakın")
 
         try:
             response = model.generate_content(f"{system_instruction} \n Soru: {prompt}")
             st.markdown(response.text)
             st.session_state.messages.append({"role": "assistant", "content": response.text})
         except Exception as e:
+
             st.error(f"Bir hata oluştu: {e}")
